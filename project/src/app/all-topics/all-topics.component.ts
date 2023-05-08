db8cdd0ad81aca19845405cb48665629f9135b8c
Ghada Sassi <sassighada5@gmail.com>
2023-05-08 15:16:52 +0200

    front end update

---
+++ project/src/app/all-topics/all-topics.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-topics',
+  templateUrl: './all-topics.component.html',
+  styleUrls: ['./all-topics.component.css']
+})
+export class AllTopicsComponent {
+
+}
--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -7,15 +7,18 @@ import { ThemeDetailsComponent } from './theme-details/theme-details.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { PostsComponent } from './posts/posts.component';
+import { AllTopicsComponent } from './all-topics/all-topics.component';
 
 const routes: Routes = [
   {path: 'themes', component: ThemeListComponent},
   {path: 'themes/new', component: CreateThemeComponent},
   {path: 'themes/edit/:id', component: UpdateThemeComponent},
-  {path: 'themes/show/:id', component: ThemeDetailsComponent},
+  {path: 'themes/show/:id', component: AllTopicsComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'topic/:id', component: PostsComponent},
 ];
 
 @NgModule({
